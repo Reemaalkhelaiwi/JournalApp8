e3d5f852a3622554d24e3096dd92092960fb9e94
reema <zkreema@icloud.com>
2025-10-21 15:35:06 +0300

    Initial Commit

---
+++ JournalApp8/JournalApp8App.swift
@@ -0,0 +1,17 @@
+//
+//  JournalApp8App.swift
+//  JournalApp8
+//
+//  Created by Reema Alkhelaiwi on 21/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct JournalApp8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
